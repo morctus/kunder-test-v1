69af6cfeef47c15e11f5bf89725803b61b2bcc99
morctus <nicanor.errazuriz.b@gmail.com>
2021-08-14 21:11:11 -0400

    Modificacion packagesjson

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-
-// Serve static files
-app.use(express.static(__dirname + '/dist/kunder-app-v1'));
-
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/kunder-app-v1/index.html'));
+var express  = require('express');
+var app      = express();                               
+var morgan = require('morgan');            
+var bodyParser = require('body-parser');    
+var cors = require('cors');
+ 
+app.use(morgan('dev'));                                        
+app.use(bodyParser.urlencoded({'extended':'true'}));            
+app.use(bodyParser.json());                                     
+app.use(cors());
+ 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
-
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+ 
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
